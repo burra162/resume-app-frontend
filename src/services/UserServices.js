e04770166256447c85e026308441a03577d67acb
Sai Kiran Banod <sai.kiran.banod@eagles.oc.edu>
2024-07-08 21:44:16 -0400

    Login implemented

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -6,5 +6,17 @@ export default {
   },
   addUser(user) {
     return apiClient.post("users", user);
-  }
+  },
+  loginUser(user) {
+    return apiClient.post("login", user.value, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        crossDomain: true,
+        Authorization:
+          "Basic " + btoa(user.value.email + ":" + user.value.password),
+      },
+    });
+  },
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,11 +20,11 @@ const user = ref({
 
 onMounted(async () => {
   localStorage.removeItem("user");
-  // if (localStorage.getItem("user") !== null) {
-  //   router.push({ name: "resumes" });
-  // }
 });
 
+function navigateToresumes() {
+  router.push({ name: "resumes" });
+}
 
 async function createAccount() {
   await UserServices.addUser(user.value)
@@ -33,6 +33,23 @@ async function createAccount() {
       snackbar.value.color = "green";
       snackbar.value.text = "Account created successfully!";
       router.push({ name: "login" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
+}
+
+async function login() {
+  await UserServices.loginUser(user)
+    .then((data) => {
+      window.localStorage.setItem("user", JSON.stringify(data.data));
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "Login successful!";
+      router.push({ name: "resumes" });
     })
     .catch((error) => {
       console.log(error);
@@ -59,28 +76,82 @@ function closeSnackBar() {
   <v-container>
     <div id="body">
       <v-card class="rounded-lg elevation-5">
+        <v-card-title class="headline mb-2">Login </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="user.email"
+            label="Email"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="user.password"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-card-text>
         <v-card-actions>
-          <v-btn variant="flat" color="secondary" @click="openCreateAccount()">Create Account</v-btn>
+          <v-btn variant="flat" color="secondary" @click="openCreateAccount()"
+            >Create Account</v-btn
+          >
           <v-spacer></v-spacer>
+
+          <v-btn variant="flat" color="primary" @click="login()">Login</v-btn>
         </v-card-actions>
+      </v-card>
+
+      <v-card class="rounded-lg elevation-5 my-8">
+        <v-card-title class="text-center headline">
+          <v-btn
+            class="ml-2"
+            variant="flat"
+            color="secondary"
+            @click="navigateToresumes()"
+          >
+            View Published resumes
+          </v-btn>
+        </v-card-title>
       </v-card>
 
       <v-dialog persistent v-model="isCreateAccount" width="800">
         <v-card class="rounded-lg elevation-5">
           <v-card-title class="headline mb-2">Create Account </v-card-title>
           <v-card-text>
-            <v-text-field v-model="user.firstName" label="First Name" required></v-text-field>
+            <v-text-field
+              v-model="user.firstName"
+              label="First Name"
+              required
+            ></v-text-field>
 
-            <v-text-field v-model="user.lastName" label="Last Name" required></v-text-field>
+            <v-text-field
+              v-model="user.lastName"
+              label="Last Name"
+              required
+            ></v-text-field>
 
-            <v-text-field v-model="user.email" label="Email" required></v-text-field>
+            <v-text-field
+              v-model="user.email"
+              label="Email"
+              required
+            ></v-text-field>
 
-            <v-text-field v-model="user.password" label="Password" required></v-text-field>
+            <v-text-field
+              v-model="user.password"
+              label="Password"
+              required
+            ></v-text-field>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn variant="flat" color="secondary" @click="closeCreateAccount()">Close</v-btn>
-            <v-btn variant="flat" color="primary" @click="createAccount()">Create Account</v-btn>
+            <v-btn
+              variant="flat"
+              color="secondary"
+              @click="closeCreateAccount()"
+              >Close</v-btn
+            >
+            <v-btn variant="flat" color="primary" @click="createAccount()"
+              >Create Account</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -89,7 +160,11 @@ function closeSnackBar() {
         {{ snackbar.text }}
 
         <template v-slot:actions>
-          <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
+          <v-btn
+            :color="snackbar.color"
+            variant="text"
+            @click="closeSnackBar()"
+          >
             Close
           </v-btn>
         </template>
