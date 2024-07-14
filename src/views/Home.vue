f42faca7e0219ecf21ae7ed2250fcea26a0f9fc4
Sai Kiran Banod <sai.kiran.banod@eagles.oc.edu>
2024-07-14 01:22:49 -0400

    Resume details completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,112 @@
 <script setup>
 import { onMounted } from "vue";
 import { ref } from "vue";
+import ResumeServices from "../services/ResumeService.js";
 
 
+const userDetails = ref({
+    firstName: "",
+    lastName: "",
+    email: "",
+    number: "",
+    address: "",
+    link1Name: "",
+    link1: "",
+    link2Name: "",
+    link2: "",
+    link3Name: "",
+    link3: "",
+    userId: "",
+});
 
-const user = ref(null);
+const isSave = ref(false);
+
+onMounted(async () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    userDetails.value.userId = user.id;
+    getUserDetails();
+});
+
+async function getUserDetails() {
+    ResumeServices.getUserDetails(userDetails.value.userId)
+        .then((data) => {
+            userDetails.value = data.data;
+            isSave.value = false;
+        })
+        .catch((error) => {
+            console.log(error);
+            isSave.value = true;
+            console.log(isSave.value);
+        });
+
+}
+
 const snackbar = ref({
     value: false,
     color: "",
     text: "",
 });
 
+const selectedTab = ref(0);
+
+function changeTab(index) {
+    selectedTab.value = index;
+}
+
+const tabs = ref([
+    "Details",
+    "Templates",
+    "Job Applications",
+]);
+
+const resumeTabs = ref([
+    "Personal Details",
+    "Professional Summary",
+    "Education",
+    "Professional Experience",
+    "Skills",
+    "Projects",
+    "Certifications"
+]);
+
+
+async function saveUserDetails() {
+    await ResumeServices.saveUserDetails(userDetails.value)
+        .then(() => {
+            snackbar.value.value = true;
+            snackbar.value.color = "green";
+            snackbar.value.text = "Details saved successfully!";
+            getUserDetails();
+        })
+        .catch((error) => {
+            console.log(error);
+            snackbar.value.value = true;
+            snackbar.value.color = "error";
+            snackbar.value.text = error.response.data.message;
+        });
+}
+
+async function updateUserDetails() {
+    await ResumeServices.updateUserDetails(userDetails.value.id, userDetails.value)
+        .then(() => {
+            snackbar.value.value = true;
+            snackbar.value.color = "green";
+            snackbar.value.text = "Details updated successfully!";
+            getUserDetails();
+        })
+        .catch((error) => {
+            console.log(error);
+            snackbar.value.value = true;
+            snackbar.value.color = "error";
+            snackbar.value.text = error.response.data.message;
+        });
+}
+
+const selectedResumeTab = ref(0);
+
+function changeResumeTab(index) {
+    selectedResumeTab.value = index;
+}
 
 function closeSnackBar() {
     snackbar.value.value = false;
@@ -19,17 +115,96 @@ function closeSnackBar() {
 
 <template>
     <v-container>
-        <div id="body">
-            <v-row align="center" class="mb-4">
-                <v-col cols="10"><v-card-title class="pl-0 text-h4 font-weight-bold">Resumes
-                    </v-card-title>
+        <v-app-bar color="primary" class="pr-3" dark>
+            <v-container>
+                <v-col>
+                    <v-row>
+                        <v-tabs bg-color="primary" fixed-tabs>
+                            <v-tab v-for="(tab, index) in tabs" :key="index" @click="changeTab(index)">
+                                {{ tab }}
+                            </v-tab>
+                        </v-tabs>
+                    </v-row>
                 </v-col>
-                
-            </v-row>
+
+            </v-container>
+        </v-app-bar>
+
+        <div id="body">
+
+            <v-container>
+                <v-row>
+                    <v-col v-if="selectedTab === 0">
+
+                        <v-tabs bg-color="primary" fixed-tabs>
+                            <v-tab v-for="(tab, index) in resumeTabs" :key="index" @click="changeResumeTab(index)">
+                                {{ tab }}
+                            </v-tab>
+                        </v-tabs>
+                        <v-card>
+                            <v-card-title>
+                                <h3>Personal Details</h3>
+                            </v-card-title>
+                            <v-card-text>
+                                <v-row>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.firstName" label="First Name"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.lastName" label="Last Name"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.email" type="email"
+                                            label="Email"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.number" type="number" label="Phone Number"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field v-model="userDetails.address" label="Address"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link1Name"
+                                            label="Link 1 Name"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link1" label="Link 1 URL"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link2Name"
+                                            label="Link 2 Name"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link2" label="Link 2 URL"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link3Name"
+                                            label="Link 3 Name"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field v-model="userDetails.link3" label="Link 3 URL"></v-text-field>
+                                    </v-col>
+                                </v-row>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn v-if="isSave" variant="flat" color="primary"
+                                    @click="saveUserDetails">Save</v-btn>
+                                <v-btn v-else variant="flat" color="primary" @click="updateUserDetails">Update</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                    <v-col v-else-if="selectedTab === 1">
+
+                    </v-col>
+                    <v-col v-else-if="selectedTab === 2">
+
+                    </v-col>
+                </v-row>
+            </v-container>
 
             <v-snackbar v-model="snackbar.value" rounded="pill">
                 {{ snackbar.text }}
-
                 <template v-slot:actions>
                     <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
                         Close
